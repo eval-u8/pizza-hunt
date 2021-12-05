38755807b07d10a635e23be8e2640df1540cd75f
Ernesto Valecillos <evalecillos@gmail.com>
2021-12-04 20:30:10 -0600

    replues

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,6 +9,30 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+    // get id of pizza
+    const searchParams = new URLSearchParams(
+        document.location.search.substring(1)
+    );
+    const pizzaId = searchParams.get("id");
+
+    // get pizzaInfo
+    fetch(`/api/pizzas/${pizzaId}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error({ message: "Something went wrong!" });
+            }
+
+            return response.json();
+        })
+        .then(printPizza)
+        .catch((err) => {
+            console.log(err);
+            alert("Cannot find a pizza with this id! Taking you back.");
+            window.history.back();
+        });
+}
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -77,36 +101,84 @@ function printReply(reply) {
 }
 
 function handleNewCommentSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const commentBody = $newCommentForm.querySelector('#comment').value;
-  const writtenBy = $newCommentForm.querySelector('#written-by').value;
+    const commentBody = $newCommentForm.querySelector("#comment").value;
+    const writtenBy = $newCommentForm.querySelector("#written-by").value;
 
-  if (!commentBody || !writtenBy) {
-    return false;
-  }
+    if (!commentBody || !writtenBy) {
+        return false;
+    }
 
-  const formData = { commentBody, writtenBy };
+    const formData = { commentBody, writtenBy };
+
+    fetch(`/api/comments/${pizzaId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            response.json();
+        })
+        .then((commentResponse) => {
+            console.log(commentResponse);
+            location.reload();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
 function handleNewReplySubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  if (!event.target.matches('.reply-form')) {
-    return false;
-  }
+    if (!event.target.matches(".reply-form")) {
+        return false;
+    }
 
-  const commentId = event.target.getAttribute('data-commentid');
+    const commentId = event.target.getAttribute("data-commentid");
 
-  const writtenBy = event.target.querySelector('[name=reply-name]').value;
-  const replyBody = event.target.querySelector('[name=reply]').value;
+    const writtenBy = event.target.querySelector("[name=reply-name]").value;
+    const replyBody = event.target.querySelector("[name=reply]").value;
 
-  if (!replyBody || !writtenBy) {
-    return false;
-  }
+    if (!replyBody || !writtenBy) {
+        return false;
+    }
 
-  const formData = { writtenBy, replyBody };
+    const formData = { writtenBy, replyBody };
+
+    fetch(`/api/comments/${pizzaId}/${commentId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            response.json();
+        })
+        .then((commentResponse) => {
+            console.log(commentResponse);
+            location.reload();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
+
+$backBtn.addEventListener("click", function () {
+    window.history.back();
+});
 
 $backBtn.addEventListener('click', function() {
   window.history.back();
@@ -114,3 +186,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
